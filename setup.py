5baeefcd699d8740c17016cba5ac865e49ff32f3
Pablo Lemos <plemos91@gmail.com>
2021-04-29 14:55:39 +0100

    Restructuring

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='SNS',
+      version='0.1',
+      description='Simple Nested Sampler',
+      url='http://github.com/storborg/funniest',
+      author='Pablo Lemos',
+      author_email='plemos91@gmail.com',
+      license='MIT',
+      packages=['SNS'],
+      zip_safe=False)

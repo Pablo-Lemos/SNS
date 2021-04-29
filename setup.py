42dcaf3358e8cb987fc57375506d5113b1b331db
Pablo Lemos <plemos91@gmail.com>
2021-04-29 14:57:26 +0100

    Updated website

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='SNS',
       version='0.1',
       description='Simple Nested Sampler',
-      url='http://github.com/storborg/funniest',
+      url='https://github.com/Pablo-Lemos/SNS.git',
       author='Pablo Lemos',
       author_email='plemos91@gmail.com',
       license='MIT',
